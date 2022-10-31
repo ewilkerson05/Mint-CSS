9e22b38af5a17bdf00b379cdbe224cbc7c51f3d5
cherringto <101591822+cherringto@users.noreply.github.com>
2022-10-31 18:56:14 -0500

    Hopefully fixed it enough to pass

---
+++ MintCSS/app.js
@@ -0,0 +1,11 @@
+function clickHere3() {
+    alert("Welcome to MintCSS!");
+}
+
+function openModal() {
+    document.getElementById("modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
